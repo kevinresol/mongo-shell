f65ced006f1a77cb3b7bfeb40344d5160cf0e0f5
kevinresol <kevinresol@gmail.com>
2016-10-11 17:13:52 +0800

    More top-level functions

--- src/mongo/shell/MongoShell.hx
+++ src/mongo/shell/MongoShell.hx
@@ -28,4 +28,18 @@ class MongoShell {
 		untyped __js__('printjson({0})', v);
 	}
 	
+	public inline static function runProgram(args:Array<String>):Int {
+		return untyped __js__('runProgram.apply(undefined, {0})', args);
+	}
+	
+	public inline static function rawMongoProgramOutput():String {
+		return untyped __js__('rawMongoProgramOutput()');
+	}
+	
+	public inline static function clearRawMongoProgramOutput():Void {
+		untyped __js__('clearRawMongoProgramOutput()');
+	}
+	public inline static function cat(v:String):String {
+		return untyped __js__('cat({0})', v);
+	}
 }
